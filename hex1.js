b95881046c724df0c3f331889ed7e7167e21ea53
Dagankol <104620350+Dagankol@users.noreply.github.com>
2026-08-31 02:02:46 +0800

    Update hex1.js

--- hex1.js
+++ hex1.js
@@ -81,9 +81,15 @@ const ciphers = [
 ];
 
 const sigAlgs = [
-  "ecdsa_secp256r1_sha256:rsa_pss_rsae_sha256:rsa_pkcs1_sha256:ecdsa_secp384r1_sha384:rsa_pss_rsae_sha384:rsa_pkcs1_sha384:rsa_pss_rsae_sha512:rsa_pkcs1_sha512",
-  "ecdsa_secp256r1_sha256:rsa_pkcs1_sha256:ecdsa_secp384r1_sha384:rsa_pkcs1_sha384:ecdsa_secp521r1_sha512:rsa_pkcs1_sha512",
-  "rsa_pss_rsae_sha256:rsa_pkcs1_sha256:ecdsa_secp256r1_sha256:rsa_pss_rsae_sha384:rsa_pkcs1_sha384:ecdsa_secp384r1_sha384:rsa_pss_rsae_sha512:rsa_pkcs1_sha512"
+   'ecdsa_secp256r1_sha256',
+  'ecdsa_secp384r1_sha384',
+  'ecdsa_secp521r1_sha512',
+  'rsa_pss_rsae_sha256',
+  'rsa_pss_rsae_sha384',
+  'rsa_pss_rsae_sha512',
+  'rsa_pkcs1_sha256',
+  'rsa_pkcs1_sha384',
+  'rsa_pkcs1_sha512'
 ];
 
 const curves = ["prime256v1", "secp384r1", "secp521r1", "X25519"];
